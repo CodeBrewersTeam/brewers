77138f3429723cb1fcf8266dc1df20e64e6d64aa
Wasim Soomro <103798599+WasimSoomro@users.noreply.github.com>
2023-08-10 03:59:28 -0400

    ResidentID

--- brewersproj/src/main/java/com/roomEase/brewersproj/repositories/ApplicationUserRepository.java
+++ brewersproj/src/main/java/com/roomEase/brewersproj/repositories/ApplicationUserRepository.java
@@ -1,6 +1,7 @@
 package com.roomEase.brewersproj.repositories;
 
 import com.roomEase.brewersproj.models.ApplicationUser;
+import com.roomEase.brewersproj.models.Residence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Set;
@@ -11,11 +12,11 @@ public interface ApplicationUserRepository extends JpaRepository<ApplicationUser
 
 //alex: need for filtering out users by ID
     List<ApplicationUser> findByHouseholdId(String householdId);
+    List<ApplicationUser> findByResidence(Residence residence);
 
 
     //this is for chores for each day of the week
     Set<ApplicationUser> findAllByIdIn(Set<Long> ids);
 
 
-//    List<ApplicationUser> findByHouseholdIdAndResidenceId(String householdId, Long currentResidenceId);
 }
